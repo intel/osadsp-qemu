99973745db17669fd4a4c113254ef60cc613d68f
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2017-05-15 12:46:53 +0100

    dsp-common.h fix

--- hw/adsp/dsp/common.h
+++ hw/adsp/dsp/common.h
@@ -24,7 +24,7 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "qemu-common.h"
-#include "target-xtensa/cpu.h"
+#include "target/xtensa/cpu.h"
 
 struct adsp_xtensa {
     XtensaCPU *cpu;
