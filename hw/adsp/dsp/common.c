e1011f85953849f0427a2980388bd3ce9cc4d7b1
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-04-24 22:19:28 -0700

    commom: add support for loading legacy hsw/bdw file formats
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- hw/adsp/dsp/common.c
+++ hw/adsp/dsp/common.c
@@ -40,7 +40,6 @@ void adsp_set_irq(struct adsp_dev *adsp, int irq, int active)
     CPUXtensaState *env = adsp->xtensa[0]->env;
     uint32_t irq_bit = 1 << irq;
 
-    qemu_mutex_lock_iothread();
     if (active) {
         env->sregs[INTSET] |= irq_bit;
     } else if (env->config->interrupt[irq].inttype == INTTYPE_LEVEL) {
@@ -48,13 +47,17 @@ void adsp_set_irq(struct adsp_dev *adsp, int irq, int active)
     }
 
     check_interrupts(env);
-
-    qemu_mutex_unlock_iothread();
 }
 
 #define SND_SOF_FW_SIG_SIZE	4
 #define SND_SOF_FW_ABI		1
 #define SND_SOF_FW_SIG		"Reef"
+
+#define SST_FW_SIG_SIZE		4
+#define SST_HSW_FW_SIGN		"$SST"
+#define SST_HSW_IRAM	1
+#define SST_HSW_DRAM	2
+#define SST_HSW_REGS	3
 
 /*
  * Firmware module is made up of 1 . N blocks of different types. The
@@ -94,7 +97,7 @@ struct snd_sof_mod_hdr {
 } __attribute__((packed));
 
 /*
- * Firmware file header.
+ * SST Firmware file header.
  */
 struct snd_sof_fw_header {
 	char sig[SND_SOF_FW_SIG_SIZE]; /* "Reef" */
@@ -103,8 +106,40 @@ struct snd_sof_fw_header {
 	uint32_t abi;		/* version of header format */
 } __attribute__((packed));
 
+
+struct snd_sst_dma_block_info {
+	uint32_t type;		/* IRAM/DRAM */
+	uint32_t size;		/* Bytes */
+	uint32_t offset;	/* Offset in I/DRAM */
+	uint32_t rsvd;		/* Reserved field */
+} __attribute__((packed));
+
+struct snd_sst_fw_module_info {
+	uint32_t persistent_size;
+	uint32_t scratch_size;
+} __attribute__((packed));
+
+struct snd_sst_fw_header {
+	char sig[SST_FW_SIG_SIZE]; /* FW signature */
+	uint32_t file_size;		/* size of fw minus this header */
+	uint32_t num_modules;		/*  # of modules */
+	uint32_t file_format;	/* version of header format */
+	uint32_t reserved[4];
+} __attribute__((packed));
+
+struct snd_sst_fw_module_header {
+	char sig[SST_FW_SIG_SIZE]; /* module signature */
+	uint32_t size;	/* size of module */
+	uint32_t num_blocks;	/* # of blocks */
+	uint16_t padding;
+	uint16_t type;	/* codec type, pp lib */
+	uint32_t entry_point;
+	struct snd_sst_fw_module_info info;
+} __attribute__((packed));
+
+
 /* generic module parser for mmaped DSPs */
-static int module_memcpy(struct adsp_dev *adsp,
+static int sof_module_memcpy(struct adsp_dev *adsp,
 				struct snd_sof_mod_hdr *module)
 {
 	const struct adsp_desc *board = adsp->desc;
@@ -141,7 +176,7 @@ static int module_memcpy(struct adsp_dev *adsp,
 			break;
 		case SOF_BLK_DATA:
 			fprintf(stdout, "data: 0x%lx size 0x%x\n",
-				board->iram.base + block->offset - board->host_iram_offset,
+				board->iram.base + block->offset - board->host_dram_offset,
 				block->size);
 			cpu_physical_memory_write(board->dram0.base + block->offset - board->host_dram_offset,
 				(void *)block + sizeof(*block), block->size);
@@ -164,15 +199,72 @@ static int module_memcpy(struct adsp_dev *adsp,
 	return 0;
 }
 
-static int check_header(struct adsp_dev *adsp,
+static int sst_module_memcpy(struct adsp_dev *adsp,
+				struct snd_sst_fw_module_header *module)
+{
+	const struct adsp_desc *board = adsp->desc;
+	struct snd_sst_dma_block_info *block;
+	uint32_t offset;
+	int count;
+
+	fprintf(stdout, "new module size 0x%x blocks 0x%x type 0x%x\n",
+		module->size, module->num_blocks, module->type);
+
+	block = (void *)module + sizeof(*module);
+
+	for (count = 0; count < module->num_blocks; count++) {
+		if (block->size == 0) {
+			fprintf(stderr,
+				 "warning: block %d size zero\n", count);
+			fprintf(stderr, " type 0x%x offset 0x%x\n",
+				 block->type, block->offset);
+			continue;
+		}
+
+		switch (block->type) {
+		case SST_HSW_REGS:
+			continue;	/* not handled atm */
+		case SST_HSW_IRAM:
+			fprintf(stdout, "text: 0x%x size 0x%x\n",
+				block->offset,
+				block->size);
+			offset = board->host_iram_offset + block->offset;
+			cpu_physical_memory_write(board->iram.base + block->offset,
+				(void *)block + sizeof(*block), block->size);
+			break;
+		case SST_HSW_DRAM:
+			fprintf(stdout, "data: 0x%lx size 0x%x\n",
+				board->iram.base + block->offset - board->host_dram_offset,
+				block->size);
+			offset = board->host_dram_offset + block->offset;
+			cpu_physical_memory_write(board->dram0.base + block->offset,
+				(void *)block + sizeof(*block), block->size);
+			break;
+		default:
+			fprintf(stderr, "error: bad type 0x%x for block 0x%x\n",
+				block->type, count);
+			return -EINVAL;
+		}
+
+		fprintf(stdout,
+			"block %d type 0x%x size 0x%x ==>  offset 0x%x\n",
+			count, block->type, block->size, offset);
+
+
+		/* next block */
+		block = (void *)block + sizeof(*block) + block->size;
+	}
+
+	return 0;
+}
+
+static int sof_check_header(struct adsp_dev *adsp,
 	struct snd_sof_fw_header *header, size_t size)
 {
 
 	/* verify FW sig */
-	if (strncmp(header->sig, SND_SOF_FW_SIG, SND_SOF_FW_SIG_SIZE) != 0) {
-		fprintf(stderr, "error: invalid firmware signature\n");
+	if (strncmp(header->sig, SND_SOF_FW_SIG, SND_SOF_FW_SIG_SIZE) != 0)
 		return -EINVAL;
-	}
 
 	/* check size is valid */
 	if (size != header->file_size + sizeof(*header)) {
@@ -188,23 +280,39 @@ static int check_header(struct adsp_dev *adsp,
 	return 0;
 }
 
-int adsp_load_modules(struct adsp_dev *adsp, void *fw, size_t size)
+static int sst_check_header(struct adsp_dev *adsp,
+	struct snd_sst_fw_header *header, size_t size)
 {
-	struct snd_sof_fw_header *header;
+
+	/* verify FW sig */
+	if (strncmp(header->sig, SST_HSW_FW_SIGN, SST_FW_SIG_SIZE) != 0)
+		return -EINVAL;
+
+	/* check size is valid */
+	if (size != header->file_size + sizeof(*header)) {
+		fprintf(stderr, "error: invalid filesize mismatch got 0x%lx expected 0x%lx\n",
+			size, header->file_size + sizeof(*header));
+		return -EINVAL;
+	}
+
+	fprintf(stdout, "header size=0x%x modules=0x%x size=%zu\n",
+		header->file_size, header->num_modules,
+		sizeof(*header));
+
+	return 0;
+}
+
+static int sof_load_modules(struct adsp_dev *adsp, void *fw,
+	struct snd_sof_fw_header *header, size_t size)
+{
 	struct snd_sof_mod_hdr *module;
 	int ret, count;
-
-	header = (struct snd_sof_fw_header *)fw;
-
-	ret = check_header(adsp, header, size);
-	if (ret < 0)
-		return ret;
 
 	/* parse each module */
 	module = fw + sizeof(*header);
 	for (count = 0; count < header->num_modules; count++) {
 		/* module */
-		ret = module_memcpy(adsp, module);
+		ret = sof_module_memcpy(adsp, module);
 		if (ret < 0) {
 			fprintf(stderr, "error: invalid module %d\n", count);
 			return ret;
@@ -213,4 +321,45 @@ int adsp_load_modules(struct adsp_dev *adsp, void *fw, size_t size)
 	}
 
 	return 0;
+}
+
+static int sst_load_modules(struct adsp_dev *adsp, void *fw,
+	struct snd_sst_fw_header *header, size_t size)
+{
+	struct snd_sst_fw_module_header *module;
+	int ret, count;
+
+	/* parse each module */
+	module = fw + sizeof(*header);
+	for (count = 0; count < header->num_modules; count++) {
+		/* module */
+		ret = sst_module_memcpy(adsp, module);
+		if (ret < 0) {
+			fprintf(stderr, "error: invalid module %d\n", count);
+			return ret;
+		}
+		module = (void *)module + sizeof(*module) + module->size;
+	}
+
+	return 0;
+}
+
+int adsp_load_modules(struct adsp_dev *adsp, void *fw, size_t size)
+{
+	struct snd_sof_fw_header *sof_header;
+	struct snd_sst_fw_header *sst_header;
+	int ret;
+
+	sof_header = (struct snd_sof_fw_header *)fw;
+	ret = sof_check_header(adsp, sof_header, size);
+	if (ret == 0)
+		return sof_load_modules(adsp, fw, sof_header, size);
+
+	sst_header = (struct snd_sst_fw_header *)fw;
+	ret = sst_check_header(adsp, sst_header, size);
+	if (ret == 0)
+		return sst_load_modules(adsp, fw, sst_header, size);
+
+	fprintf(stderr, "error: invalid firmware signature\n");
+	return -EINVAL;
 }
